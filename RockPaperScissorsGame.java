459d69f68645dd31aea16543149eee62be1075dd
Barbara Santana <86841973+barbarasantana1@users.noreply.github.com>
2024-12-07 19:17:26 -0300

    RockPaperScissorsGame.java

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -1,31 +1,25 @@
-class Choices {
-    int rock = 0;
-    int paper = 1;
-    int scissors = 2;
-    String[] choicesgame = {"rock", "paper", "scissors"};
-}
-
 class RockPaperScissors {
-    void startGame() {
-        int computerChoice = (int) (Math.random() * 3);
-        int playerChoice = (int) (Math.random() * 3);
+    String[] choicesList = {"rock", "paper", "scissors"};
+    int computerChoice = (int) (Math.random() * 3);
+    int playerChoice = (int) (Math.random() * 3);
 
-        Choices listchoices = new Choices();
+    void startGame() {
+        String resultMessage = "computer: " + choicesList[computerChoice] + " you: " + choicesList[playerChoice];
 
         if (playerChoice == computerChoice) {
-            System.out.println("computer: " + listchoices.choicesgame[computerChoice] + " you: " + listchoices.choicesgame[playerChoice]);
+            System.out.println(resultMessage);
             System.out.println("It's a draw!");
         } else if (playerChoice == 0 && computerChoice == 2) {
-            System.out.println("computer: " + listchoices.choicesgame[computerChoice] + " you: " + listchoices.choicesgame[playerChoice]);
+            System.out.println(resultMessage);
             System.out.println("You won!");
         } else if (playerChoice == 1 && computerChoice == 0) {
-            System.out.println("computer: " + listchoices.choicesgame[computerChoice] + " you: " + listchoices.choicesgame[playerChoice]);
+            System.out.println(resultMessage);
             System.out.println("You won!");
         } else if (playerChoice == 2 && computerChoice == 1) {
-            System.out.println("computer: " + listchoices.choicesgame[computerChoice] + " you: " + listchoices.choicesgame[playerChoice]);
+            System.out.println(resultMessage);
             System.out.println("You won!");
         } else {
-            System.out.println("computer: " + listchoices.choicesgame[computerChoice] + " you: " + listchoices.choicesgame[playerChoice]);
+            System.out.println(resultMessage);
             System.out.println("You lost!");
         }
     }
